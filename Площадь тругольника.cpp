b2e2555cc48b2d643f8aad34d36cd455312fb610
JustChik <103586184+JustChik@users.noreply.github.com>
2022-04-13 23:36:18 +0300

    Add files via upload

---
+++ Площадь тругольника.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main() {
+    double a, b, c, p, s;
+    cin >> a >> b >> c;
+    p = (a + c + b) / 2;
+    s = sqrt(p*(p - a) * (p - b) * (p - c));
+    cout << s;
+    return 0;
+}
